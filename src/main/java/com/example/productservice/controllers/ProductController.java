35f6e110890ae6b213d4b2c17622729deb0f11bf
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-06-16 12:08:00 +0530

    removed commented code

--- src/main/java/com/example/productservice/controllers/ProductController.java
+++ src/main/java/com/example/productservice/controllers/ProductController.java
@@ -1,15 +1,12 @@
 package com.example.productservice.controllers;
 
-import com.example.productservice.dtos.ExceptionDto;
 import com.example.productservice.exceptions.ProductNotFoundException;
 import com.example.productservice.models.Product;
 import com.example.productservice.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -48,13 +45,4 @@ public class ProductController {
 //
 //    }
 
-//    @ExceptionHandler(ProductNotFoundException.class)
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ResponseBody
-//    private ExceptionDto handleProductNotFoundException(ProductNotFoundException productNotFoundException) {
-//        ExceptionDto exceptionDto = new ExceptionDto();
-//        exceptionDto.setMessage(productNotFoundException.getMessage());
-//        exceptionDto.setStatus("FAILURE");
-//        return exceptionDto;
-//    }
 }
