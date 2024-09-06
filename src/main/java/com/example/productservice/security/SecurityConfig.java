57dc2d32611e749eab9e6a537d412e32f3801637
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-09-07 01:52:22 +0530

    [API RESTRICTION]access product API based on scope or role

--- src/main/java/com/example/productservice/security/SecurityConfig.java
+++ src/main/java/com/example/productservice/security/SecurityConfig.java
@@ -10,9 +10,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http    .csrf().disable()  // Disable CSRF protection if it's not needed
+        http
                 .authorizeHttpRequests(authorize -> authorize
-                          .anyRequest().authenticated() // to enable authentication for product requests
+                                .requestMatchers("/products/**").hasAuthority("SCOPE_ADMIN") // only SCOPE_ADMIN can access all API
+                          .anyRequest().permitAll() // to enable authentication for product requests
                         //.anyRequest().permitAll() // to enable products request unauthenticated
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
