5a84bdc54027f60bcac3fd3590030d1f1fab3e79
Vahid Akhtar <akhtarvahid543@gmail.com>
2024-09-11 01:18:37 +0530

    sample controller created for health check

---
+++ src/main/java/com/example/productservice/controllers/SampleController.java
@@ -0,0 +1,17 @@
+package com.example.productservice.controllers;
+
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@NoArgsConstructor
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+    @GetMapping("/{name}")
+    public String getProduct(@PathVariable("name") String name) {
+        return "Hey " + name +"."+ " your api status is UP";
+    }
+}
